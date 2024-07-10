513fcc663291f58d4af0c4782dccae32eee19e8b
eacaps <eacaps@gmail.com>
2024-07-10 19:11:59 -0400

    feat added bin script to create updated word list

---
+++ src/fiveforks-parser.ts
@@ -0,0 +1,20 @@
+import { HTMLElement, parse } from 'node-html-parser';
+
+export const getData = async () => {
+    const data = await fetch('https://www.fiveforks.com/wordle/block/');
+    const html = await data.text();
+    const document = parse(html);
+    return getWords(document);
+}
+
+export const getWords = (document: HTMLElement) => {
+    const words:string[] = [];
+    const block = document.querySelector('.entry-content > div');
+    if(!block || !block.childNodes) return words;
+    for(const element of block.childNodes) {
+        if(element.textContent) {
+            words.push(...element.textContent.split(" ").filter(word => word != '\n'))
+        }
+    }
+    return words;
+}
